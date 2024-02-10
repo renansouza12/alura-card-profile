0ad3afaa8b5e69c0ea4e4a21895e3d49614869c5
RenanSouza <renansouza0122@gmail.com>
2024-02-10 18:49:27 -0300

    media queries  added

--- js/api.js
+++ js/api.js
@@ -2,7 +2,8 @@ const title = document.querySelector('.title');
 const courses = document.querySelector('.courses');
 const steps = document.querySelector('.steps');
 const userId = document.querySelector('.numId');
-const degree = document.querySelector('.degree')
+const degree = document.querySelector('.degree');
+
 const url = 'https://www.alura.com.br/api/dashboard/0af72c83fba398987e3f08a721ad23190b10a1f95c9764b6e8b070496a23f3ca';
 
 async function getUser(){
