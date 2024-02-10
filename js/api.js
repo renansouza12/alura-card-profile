ba29bd2c43ae3f69edfd28c6c57843545e23cb49
RenanSouza <renansouza0122@gmail.com>
2024-02-10 11:41:46 -0300

    added async/await

--- js/api.js
+++ js/api.js
@@ -4,30 +4,25 @@ const totalSteps = document.querySelector('.steps');
 
 const url = 'https://www.alura.com.br/api/dashboard/0af72c83fba398987e3f08a721ad23190b10a1f95c9764b6e8b070496a23f3ca';
 
- fetch(url)
-    .then(response => {
-      return response.json();
-    })  
-    .then(data => {
-     return data
-    })
-    .then(results =>{
-     getData(results);
-    })
-    .catch(error => {
-      console.error('Fetch error:', error);
-    });
-
-function getData(results){
-  const details = results.guides['0'];
-  console.log(details);
-  const code = details.code;
-  const courses = details.totalCourses;
-  const steps = details.totalSteps;
+async function getUser(){
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.guides['0'];
+  return results;
+}
 
 
+async function getData(){
+  const data = await getUser();
+  const { code, totalCourses, totalSteps } = data;
+  displayInformations(code, totalCourses, totalSteps)
+}
+
+getData();
+
+function displayInformations (code, courses, steps){
   title.innerHTML = code;
   totalCourses.innerHTML = courses;
   totalSteps.innerHTML = steps;
-  
+
 }
